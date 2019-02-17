4cb75018d4738442aea26f902309210fc14ead88
c5apple <c5apple_party@yahoo.co.jp>
2019-02-17 23:09:07 +0900

    シェア イベントトラッキング

--- src/app/shared/component/share-modal/share-modal.component.ts
+++ src/app/shared/component/share-modal/share-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GaService } from 'shared/service/ga';
 
 /**
  * シェアコンポーネント
@@ -10,7 +11,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShareModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private gaService: GaService
+  ) { }
 
   ngOnInit() {
     // モーダル
@@ -21,6 +24,9 @@ export class ShareModalComponent implements OnInit {
    * Facebookでシェア
    */
   shareFacebook(): void {
+    // Tracking
+    this.gaService.sendEvent('share', 'link', 'click', 'Facebook');
+
     const url = 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(location.href);
     window.open(url, '_blank', 'noopener');
   }
@@ -29,6 +35,9 @@ export class ShareModalComponent implements OnInit {
    * Twitterでシェア
    */
   shareTwitter(): void {
+    // Tracking
+    this.gaService.sendEvent('share', 'link', 'click', 'Twitter');
+
     // TODO シェア内容
     const text = encodeURIComponent(document.title);
     const url = 'https://twitter.com/share?text=' + text + '&url=' + encodeURIComponent(location.href);
@@ -39,6 +48,9 @@ export class ShareModalComponent implements OnInit {
    * LINEでシェア
    */
   shareLine(): void {
+    // Tracking
+    this.gaService.sendEvent('share', 'link', 'click', 'LINE');
+
     // TODO シェア内容
     const text = encodeURIComponent(document.title) + '%20' + encodeURIComponent(location.href);
     const url = 'https://line.me/R/msg/text/?' + text;
@@ -49,6 +61,9 @@ export class ShareModalComponent implements OnInit {
    * メールでシェア
    */
   shareMail(): void {
+    // Tracking
+    this.gaService.sendEvent('share', 'link', 'click', 'メール');
+
     // TODO メール内容
     const subject = '';
     const body = encodeURIComponent(document.title) + '%20' + encodeURIComponent(location.href);
@@ -60,6 +75,9 @@ export class ShareModalComponent implements OnInit {
    * リンクをコピー
    */
   copyLink(): void {
+    // Tracking
+    this.gaService.sendEvent('share', 'link', 'click', 'コピー');
+
     document.addEventListener('copy', (e: ClipboardEvent) => {
       e.preventDefault();
       e.clipboardData.setData('text/plain', location.href);
