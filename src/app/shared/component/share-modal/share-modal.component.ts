998ee7245e203a42f8f6fe9c6768f219b76cdf2d
c5apple <c5apple_party@yahoo.co.jp>
2019-02-17 22:57:40 +0900

    シェアリンク設定

--- src/app/shared/component/share-modal/share-modal.component.ts
+++ src/app/shared/component/share-modal/share-modal.component.ts
@@ -17,27 +17,56 @@ export class ShareModalComponent implements OnInit {
     window['$']('.modal').modal();
   }
 
-  test(): void {
-    alert(window.navigator['share']);
+  /**
+   * Facebookでシェア
+   */
+  shareFacebook(): void {
+    const url = 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(location.href);
+    window.open(url, '_blank', 'noopener');
+  }
 
-    if (typeof window.navigator['share'] === 'undefined') {
-      return;
-    }
+  /**
+   * Twitterでシェア
+   */
+  shareTwitter(): void {
+    // TODO シェア内容
+    const text = encodeURIComponent(document.title);
+    const url = 'https://twitter.com/share?text=' + text + '&url=' + encodeURIComponent(location.href);
+    window.open(url, '_blank', 'noopener');
+  }
 
-    var shareData = {
-      title: document.querySelector('title').textContent,
-      text: document.querySelector('meta[name="description"]').getAttribute('content'),
-      url: location.href
-    };
+  /**
+   * LINEでシェア
+   */
+  shareLine(): void {
+    // TODO シェア内容
+    const text = encodeURIComponent(document.title) + '%20' + encodeURIComponent(location.href);
+    const url = 'https://line.me/R/msg/text/?' + text;
+    window.open(url, '_blank', 'noopener');
+  }
 
-    navigator['share'](shareData)
-      .then(function () {
-        // シェア完了後の処理
-        alert('success!!');
-      })
-      .catch(function (error) {
-        // シェア失敗時の処理
-        alert('error!!');
-      });
+  /**
+   * メールでシェア
+   */
+  shareMail(): void {
+    // TODO メール内容
+    const subject = '';
+    const body = encodeURIComponent(document.title) + '%20' + encodeURIComponent(location.href);
+    const url = 'mailto:?subject=' + subject + '&amp;body=' + body;
+    location.href = url;
+  }
+
+  /**
+   * リンクをコピー
+   */
+  copyLink(): void {
+    document.addEventListener('copy', (e: ClipboardEvent) => {
+      e.preventDefault();
+      e.clipboardData.setData('text/plain', location.href);
+    }, { once: true });
+    document.execCommand('copy');
+
+    window['M'].toast({ html: 'リンクがクリップボードにコピーされました。' });
+    window['M'].Modal.getInstance(document.getElementById('share-modal')).close(); // TODO インスタンスを@Inputしたほうが良い？
   }
 }
