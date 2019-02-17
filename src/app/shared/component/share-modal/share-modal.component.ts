54f6460ed6fd07a1e7cbe43d20e24c0ba9555075
c5apple <c5apple_party@yahoo.co.jp>
2019-02-17 17:59:30 +0900

    シェアテスト

--- src/app/shared/component/share-modal/share-modal.component.ts
+++ src/app/shared/component/share-modal/share-modal.component.ts
@@ -17,4 +17,27 @@ export class ShareModalComponent implements OnInit {
     window['$']('.modal').modal();
   }
 
+  test(): void {
+    alert(window.navigator['share']);
+
+    if (typeof window.navigator['share'] === 'undefined') {
+      return;
+    }
+
+    var shareData = {
+      title: document.querySelector('title').textContent,
+      text: document.querySelector('meta[name="description"]').getAttribute('content'),
+      url: location.href
+    };
+
+    navigator['share'](shareData)
+      .then(function () {
+        // シェア完了後の処理
+        alert('success!!');
+      })
+      .catch(function (error) {
+        // シェア失敗時の処理
+        alert('error!!');
+      });
+  }
 }
