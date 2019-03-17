eca34259995f0da44d9ce46a08aba1e6ebec8656
c5apple <c5apple_party@yahoo.co.jp>
2019-03-17 13:20:52 +0900

    アカウント作成(途中)

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginForm } from '../login/login-form';
 
 /**
  * アカウント作成画面
@@ -11,9 +13,20 @@ import { Router } from '@angular/router';
 })
 export class SignupComponent implements OnInit {
 
+  /** 入力フォーム */
+  form: FormGroup;
+
+  /** APIバリデーション失敗 */
+  isInValid: boolean;
+  /** APIエラー */
+  isError: boolean;
+
   constructor(
+    private formBuilder: FormBuilder,
     private router: Router
-  ) { }
+  ) {
+    this.form = this.formBuilder.group(LoginForm.validators);
+  }
 
   ngOnInit() {
     // TODO 未作成のため暫定でログイン画面へ
--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { SignupComponent } from './signup.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [SignupComponent],
   exports: [SignupComponent]
