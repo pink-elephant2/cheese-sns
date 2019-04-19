90a3e45ffcb685cc42f712f8ff144958aa64b7cd
c5apple <c5apple_party@yahoo.co.jp>
2019-04-19 22:21:25 +0900

    #18 アカウント画像ディレクティブ作成

---
+++ src/app/shared/directive/account-image/account-image.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AccountImageDirective } from './account-image.directive';
+
+describe('AccountImageDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AccountImageDirective();
+    expect(directive).toBeTruthy();
+  });
+});
