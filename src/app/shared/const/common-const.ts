22267535376414bd052408ec9ff0935f6d9d86e7
c5apple <c5apple_party@yahoo.co.jp>
2020-03-14 01:18:49 +0900

    #29 利用規約

--- src/app/shared/const/common-const.ts
+++ src/app/shared/const/common-const.ts
@@ -4,6 +4,9 @@ export const APP_TITLE = '撮ろっとチーズ';
 /** 説明文 */
 export const APP_DESCRIPTION = 'チーズ大好きな人集まれ！チーズ民のチーズ民によるチーズ民のためのSNSです。';
 
+/** 製作者 */
+export const APP_PRODUCER = '撮ろっとチーズ制作委員会';
+
 /**
  * 共通定数
  */
