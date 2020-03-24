a8c10df22a0f837d5e1dc292b03a8548ded9e5ca
c5apple <c5apple_party@yahoo.co.jp>
2020-03-24 21:38:46 +0900

    ヘッダー アカウント画像がない場合の不具合

--- src/app/shared/component/header/header.module.ts
+++ src/app/shared/component/header/header.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { AppRoutingModule } from '../../../app-routing.module';
 import { environment } from 'env/environment';
+import { AccountImageModule } from 'shared/directive/account-image';
 import { ThemeColorModule } from 'shared/directive/theme-color';
 
 @NgModule({
   imports: [
     CommonModule,
     AppRoutingModule,
+    AccountImageModule,
     ThemeColorModule
   ],
   declarations: [HeaderComponent],
